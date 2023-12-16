05102512e11671f2787f3ee13ce65c9783286605
Eduardo Nascimento <eduardosgn19@gmail.com>
2023-12-16 18:53:42 -0300

    first commit

---
+++ app/(dashboard)/_components/Logo.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Logo() {
+    return <Image width={40} height={40} alt="LMS Logo" src="/logo.svg" />;
+}
---
+++ app/(dashboard)/search/page.tsx
@@ -0,0 +1,7 @@
+export default function SearchPage() {
+    return (
+        <div>
+            <p>Search page</p>
+        </div>
+    );
+}
